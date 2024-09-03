8a63e3e0347cc2639b3cc96b7cddf7109f114e41
rochlin <rochlin.dev@gmail.com>
2024-09-03 22:59:25 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,68 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, squooshImageService, passthroughImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+import compress from 'astro-compress';
+import astrowind from './vendor/integration';
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter.mjs';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const hasExternalScripts = false;
+const whenExternalScripts = (items = []) => hasExternalScripts ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+  //output: 'hybrid',
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), icon({
+    include: {
+      tabler: ['*'],
+      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
+    }
+  }), ...whenExternalScripts(() => partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })), compress({
+    CSS: true,
+    HTML: {
+      'html-minifier-terser': {
+        removeAttributeQuotes: false
+      }
+    },
+    Image: false,
+    JavaScript: true,
+    SVG: false,
+    Logger: 1
+  }), astrowind({
+    config: './src/config.yaml'
+  })],
+  image: {
+    service: passthroughImageService(),
+    domains: ['cdn.pixabay.com']
+  },
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin]
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
+});
